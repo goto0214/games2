f500061db9aaf47699da0428810eafcb05687cb0
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-24 14:37:56 +0000

    [update]

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,48 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.all
+    @game = Game.new
+    @user = current_user
   end
 
   def show
+    @game = Game.find(params[:id])
+    @create = Game.new
+    @user = @game.user
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.user_id = current_user.id
+    @game.save
+    redirect_to game_path(@game)
   end
 
   def edit
+    @game = Game.find(params[:id])
+    if @game.user == current_user
+      render :edit
+    else
+      redirect_to games_path
+    end
   end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.user_id = current_user
+    @game.update(game_params)
+    redirect_to game_path(@game)
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to games_path
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:title, :genre, :detail)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get "home/about" => "homes#about", as: "about"
-  resources :users
+  resources :users, only: [:index, :edit, :show, :update]
+  resources :games, only: [:index, :ahow, :create, :edit, :update, :destroy]
 end
