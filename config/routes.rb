1deb21856b089a2d30b8ce2786d27a14e10c60ea
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-24 10:26:52 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'games/index'
-  get 'games/show'
-  get 'games/edit'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+  get "home/about" => "homes#about", as: "about"
+  resources :users
 end
