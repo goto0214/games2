556eb9beaa990a5845ad8059659e71b6fab86d86
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-24 12:52:27 +0000

    [update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+  def indx
+    @users = User.all
+    @user = current_user
+    @game = Game.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @game = Game.new
+    @games = @user.games.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
