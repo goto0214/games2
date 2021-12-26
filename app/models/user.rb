bdcaf51c22a9b04877497dc56a1f7fd9c9db3039
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-26 15:40:27 +0000

    [update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,22 @@ class User < ApplicationRecord
   has_many :games, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+
+  has_many :followers,
+  class_name: 'Relationship',
+  foreign_key: 'follower_id',
+  dependent: :destroy,
+  inverse_of: :follower
+
+  has_many :followings,
+  class_name: 'Relationship',
+  foreign_key: 'following_id',
+  dependent: :destroy,
+  inverse_of: :following
+
+  has_many :following_users, through: :followers, source: :following
+  has_many :follower_users, through: :followings, source: :follower
+
   attachment :profile_image
 
   validates :name, presence: true
