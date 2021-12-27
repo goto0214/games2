39818475aba032936b7ad8ee20b685fd5ec7a3ec
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-27 11:29:59 +0000

    [update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,25 @@ class User < ApplicationRecord
   has_many :games, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed #自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower #自分をフォローしている人
 
-  has_many :followers,
-  class_name: 'Relationship',
-  foreign_key: 'follower_id',
-  dependent: :destroy,
-  inverse_of: :follower
+  #ユーザーをフォローする
+  def follow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
 
-  has_many :followings,
-  class_name: 'Relationship',
-  foreign_key: 'following_id',
-  dependent: :destroy,
-  inverse_of: :following
+  #ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
 
-  has_many :following_users, through: :followers, source: :following
-  has_many :follower_users, through: :followings, source: :follower
+  #フォローしていればtrueを返す
+  def folloowing?(user)
+    folloowing_user.include?(user)
+  end
 
   attachment :profile_image
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get "home/about" => "homes#about", as: "about"
-  resources :users, only: [:index, :edit, :show, :update]
+  resources :users, only: [:index, :edit, :show, :update] do
+    member do
+      get :follows, :followers
+    end
+    resources :relationships, only: [:create, :destroy]
+  end
   resources :games, only: [:index, :show, :create, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
